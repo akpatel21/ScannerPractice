6216f5cf18e01d7fd7194fe57468404d838813cc
akpatel21 <43002673+akpatel21@users.noreply.github.com>
2018-10-03 08:38:44 -0400

    Add files via upload

---
+++ src/Favorites.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Favorites 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner (System.in);
+		System.out.print("Please entetr your name:  ");
+		String name = keyboard.nextLine();
+		
+	}
+
+}
